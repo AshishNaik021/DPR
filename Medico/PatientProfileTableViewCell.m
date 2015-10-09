83196aeb20e2077ab47cc4fe83fa8d6eae59b022
APPLE <apple@APPLEs-Mac-mini.local>
2015-10-09 18:07:15 +0530

    Patient profile

---
+++ Medico/PatientProfileTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PatientProfileTableViewCell.m
+//  Medico
+//
+//  Created by APPLE on 09/10/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+#import "PatientProfileTableViewCell.h"
+
+@implementation PatientProfileTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Medico/PatientProfileViewController.m
@@ -0,0 +1,91 @@
+//
+//  PatientProfileViewController.m
+//  Medico
+//
+//  Created by APPLE on 09/10/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+#import "PatientProfileViewController.h"
+#import "DoctorLandingPageView.h"
+#import "PatientProfileTableViewCell.h"
+
+@interface PatientProfileViewController ()
+
+@end
+
+@implementation PatientProfileViewController
+
+- (void) homePage:(id)sender{
+    DoctorLandingPageView *DoctorHome =
+    [self.storyboard instantiateViewControllerWithIdentifier:@"DoctorHome"];
+    [self.navigationController pushViewController:DoctorHome animated:YES];
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    gridViewTable.backgroundColor = [UIColor clearColor];
+    UIImage *myImage = [UIImage imageNamed:@"home.png"];
+    UIBarButtonItem *homeButton = [[UIBarButtonItem alloc]  initWithImage:myImage style:UIBarButtonItemStylePlain target:self action:@selector(homePage:)];
+    NSArray *buttonArr = [[NSArray alloc] initWithObjects:homeButton, nil];
+    self.navigationItem.rightBarButtonItems = buttonArr;
+    
+    self.navigationItem.title = @"Patient Profile";
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleDone target:nil action:nil];
+    [[self navigationItem] setBackBarButtonItem:backButton];
+    NSString *fileName = [[NSBundle mainBundle] pathForResource:@"Patient" ofType:@"json"];
+    NSString *myJson = [[NSString alloc] initWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:NULL];
+    NSError *error = nil;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[myJson dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
+    _patientArr = [json valueForKeyPath:@"PatientProfile"];
+    // Do any additional setup after loading the view.
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return _patientArr.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"TableCell";
+    PatientProfileTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    //for(int count = 0;count<_arr.count;count++){
+    int row = [indexPath row];
+    cell.patientNameLabel.text = [[_patientArr objectAtIndex:row] objectForKey:@"name"];
+    cell.specialistLabel.text = [[_patientArr objectAtIndex:row] objectForKey:@"speciality"];
+    cell.lastVisitedLabel.text = [[_patientArr objectAtIndex:row] objectForKey:@"lastVisited"];
+     cell.patientPicture.image = [UIImage imageNamed:@"patientProfile.png"];
+    return cell;
+    
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
