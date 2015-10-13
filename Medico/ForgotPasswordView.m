d85546b87cdf9e000cd709544b97860cf0808c8b
APPLE <apple@APPLEs-Mac-mini.local>
2015-10-13 15:14:40 +0530

    Forgot Password Changes....

--- Medico/ForgotPasswordView.m
+++ Medico/ForgotPasswordView.m
@@ -13,8 +13,13 @@
 @end
 
 @implementation ForgotPasswordView
+@synthesize emailField;
+@synthesize sendButton;
+@synthesize backgroundImage;
+@synthesize mobileNoField;
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
@@ -22,6 +27,32 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)validateEmail:(NSString*)checkString
+{
+    BOOL stricterFilter = NO;
+    NSString *stricterFilterString = @"[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}";
+    NSString *laxString = @".+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*";
+    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:checkString];
+}
+- (BOOL)validatePhone:(NSString *)phoneNumber
+{
+    NSString *phoneRegex = @"^((\\+)|(00))[0-9]{6,14}$";
+    NSPredicate *phoneTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", phoneRegex];
+    
+    return [phoneTest evaluateWithObject:phoneNumber];
+}
+- (IBAction)validate:(id)sender {
+    if ([emailField.text isEqualToString:@""] && [mobileNoField.text isEqualToString:@""]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning!" message:@"Please Enter Email ID or Mobile Number for Password retrieval" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+    else{
+        
+    }
 }
 
 /*
