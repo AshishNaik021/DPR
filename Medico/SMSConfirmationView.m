2af639491066ce1665e4ef8251e6d17f3aa40f0c
Poonam Choudhari <poonam6choudhari@gmail.com>
2015-10-21 13:24:00 +0530

    register

--- Medico/SMSConfirmationView.m
+++ Medico/SMSConfirmationView.m
@@ -16,8 +16,16 @@
 @implementation SMSConfirmationView
 
 @synthesize data = _data;
+@synthesize smsRegistrationCodeField;
+@synthesize emailRegistrationCodeField;
+@synthesize smsResendButton;
+@synthesize emailResendButton;
+@synthesize registrationConfirmedButton;
+@synthesize registrationCode;
+@synthesize returnString;
 
 - (void)viewDidLoad {
+    registrationCode = @"";
     [super viewDidLoad];
     NSLog(@"Data dic: %@",_data);
     NSError *error;
@@ -29,7 +37,7 @@
         NSLog(@"Got an error: %@", error);
     } else {
         NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        NSLog(@"%2",jsonString);
+        NSLog(@"jsonstring %@",jsonString);
     }
     // Do any additional setup after loading the view.
 }
@@ -49,10 +57,167 @@
  }
  */
 
+-(void) errorMessageEnterCode{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!" message:@"Please enter Registration Code." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
+}
+-(void)errorMessageDifferentCode{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!" message:@"Please enter Unique Registration Code." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
+}
+-(void)errorMesasgeWrongCode{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!" message:@"Please enter valid Registration Code." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
+}
 - (IBAction)smsResend:(id)sender {
 }
 - (IBAction)emailResend:(id)sender {
 }
+-(void)parseJSON : (NSString *)responseData{
+    NSString * jsonString = responseData;
+    NSLog(@"responseData %@",responseData);
+    NSLog(@"jsonString %@",jsonString);
+    if ([jsonString intValue]) {
+        SMSConfirmationView *confirmationView =
+        [self.storyboard instantiateViewControllerWithIdentifier:@"SMSConfirmationView"];
+        [self.navigationController pushViewController:confirmationView animated:YES];
+    }
+    else{
+        //NSStringEncoding  encoding;
+        NSData * jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+        NSError * error=nil;
+        NSDictionary * parsedData = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
+        NSLog(@"NSDictionery:%@",parsedData);
+        NSString *message = [NSString stringWithFormat:[parsedData valueForKey:@"message"]];
+        //NSLog(@"userType:%@",[parsedData valueForKey:@"type"]);
+        if ([message isEqualToString:@"User Already Exist!"]) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning!"
+                                                            message:@"User already exists. Please try to Login!"
+                                                           delegate:self
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+            [alert show];
+            
+        }
+        //    else if ([userType isEqualToString:@"Patient"]){
+        //        PatientLandingPageView *patientHome =
+        //        [self.storyboard instantiateViewControllerWithIdentifier:@"PatientHome"];
+        //        [self.navigationController pushViewController:patientHome animated:YES];
+        //    }
+        //    else {
+        //        AssistantLandingPageView *assistantHome =
+        //        [self.storyboard instantiateViewControllerWithIdentifier:@"AssistantHome"];
+        //        [self.navigationController pushViewController:assistantHome animated:YES];
+        //    }
+    }
+}
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"touchesBegan:withEvent:");
+    [self.view endEditing:YES];
+}
+-(void)registerDoctor{
+    //returnString = @"";
+    
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: nil delegateQueue: [NSOperationQueue mainQueue]];
+    
+    NSURL * url = [NSURL URLWithString:@"http://139.162.31.36:9000/registerDoctor"];
+    NSMutableURLRequest * urlRequest = [NSMutableURLRequest requestWithURL:url];
+    //NSString * params =[NSString stringWithFormat:@"\{\"bloodGroup\":%@,\"cloudLoginId\":%@,\"cloudLoginPassword\":%@,\"cloudType\":%@,\"dateOfBirth\":\"%@\",\"emailID\":\"%@\",\"gender\":\"%@\",\"location\":\"%@\",\"mobileNumber\":\"%@\",\"name\":\"%@\",\"password\":\"%@\",\"speciality\":\"%@\"\}",[_data objectForKey:@"bloodGroup"],[_data objectForKey:@"cloudLoginId"],[_data objectForKey:@"cloudLoginPassword"],[_data objectForKey:@"cloudType"],[_data objectForKey:@"dateOfBirth"],[_data objectForKey:@"emailID"],[_data objectForKey:@"gender"],[_data objectForKey:@"location"],[_data objectForKey:@"mobileNumber"],[_data objectForKey:@"name"],[_data objectForKey:@"password"],[_data objectForKey:@"speciality"]];
+    NSString * params =[NSString stringWithFormat:@"\{\"bloodGroup\":\"A+\",\"cloudLoginId\":\"\",\"cloudLoginPassword\":\"\",\"cloudType\":\"3\",\"dateOfBirth\":\"2015-9-22\",\"emailID\":\"%@\",\"gender\":\"Male\",\"location\":\"pune\",\"mobileNumber\":\"+911234567890\",\"name\":\"rajiv\",\"password\":\"rajiv\",\"speciality\":\"pediatric\"}",[_data objectForKey:@"emailID"]];
+    NSLog(@"params %@",params);
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [urlRequest setHTTPBody:[params dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSURLSessionDataTask * dataTask =[defaultSession dataTaskWithRequest:urlRequest
+                                                       completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                           NSLog(@"Response:%@ Error : %@\n", response, error);
+                                                           //NSLog(@"Response Code:%@",[response valueForKey:@"status code"]);
+                                                           if(error == nil)
+                                                           {
+                                                               returnString = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+                                                               NSLog(@"Data = %@",returnString);
+                                                               NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+                                                               NSLog(@"response status code: %ld", (long)[httpResponse statusCode]);
+                                                               if ([httpResponse statusCode] == 200) {
+                                                                   [self parseJSON:returnString];
+                                                               } else {
+                                                                   //[self reportError:[httpResponse statusCode]];
+                                                                   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!"
+                                                                                                                   message:@"An error occured. Please try again later."
+                                                                                                                  delegate:self
+                                                                                                         cancelButtonTitle:@"OK"
+                                                                                                         otherButtonTitles:nil];
+                                                                   [alert show];
+                                                                   
+                                                               }
+                                                           }
+                                                           else{
+                                                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!"
+                                                                                                               message:@"An error occured. Please try again later."
+                                                                                                              delegate:self
+                                                                                                     cancelButtonTitle:@"OK"
+                                                                                                     otherButtonTitles:nil];
+                                                               [alert show];
+                                                           }
+                                                           
+                                                       }];
+    [dataTask resume];
+    
+}
+-(void)verify:(NSString *)regCode{
+    if ([registrationCode isEqualToString:@"1234"]) {
+        [self registerDoctor];
+    }
+    else{
+        [self errorMesasgeWrongCode];
+    }
+    
+    
+    
+    //verfify reg code here.
+    
+    //    NSMutableURLRequest *request =
+    //    [NSMutableURLRequest requestWithURL:[NSURL
+    //                                         URLWithString:@"http://139.162.31.36:9000/getVerificationCode"]
+    //                            cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+    //                        timeoutInterval:10
+    //     ];
+    //
+    //    [request setHTTPMethod: @"GET"];
+    //
+    //    NSError *requestError = nil;
+    //    NSURLResponse *urlResponse = nil;
+    //
+    //
+    //    NSData *response1 =
+    //    [NSURLConnection sendSynchronousRequest:request
+    //                          returningResponse:&urlResponse error:&requestError];
+}
+
 - (IBAction)registrationConfirmed:(id)sender {
+    if(![smsRegistrationCodeField.text isEqualToString:@""] && ![emailRegistrationCodeField.text isEqualToString:@""]){
+        if ([smsRegistrationCodeField.text isEqualToString:emailRegistrationCodeField.text]) {
+            registrationCode = [NSString stringWithFormat:smsRegistrationCodeField.text];
+            [self verify:registrationCode];
+        }
+        else{
+            [self errorMessageDifferentCode];
+        }
+        
+    }
+    else if ([smsRegistrationCodeField.text isEqualToString:@""] && [emailRegistrationCodeField.text isEqualToString:@""]) {
+        [self errorMessageEnterCode];
+    }
+    else if (![smsRegistrationCodeField.text isEqualToString:@""] && [emailRegistrationCodeField.text isEqualToString:@""]){
+        registrationCode = [NSString stringWithFormat:smsRegistrationCodeField.text];
+        [self verify:registrationCode];
+    }
+    else if ([smsRegistrationCodeField.text isEqualToString:@""] && ![emailRegistrationCodeField.text isEqualToString:@""]){
+        registrationCode = [NSString stringWithFormat:emailRegistrationCodeField.text];
+        [self verify:registrationCode];
+    }
+    
 }
 @end
