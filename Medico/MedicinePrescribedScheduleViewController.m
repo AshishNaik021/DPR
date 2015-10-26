322902c609ad1b6e86dc9690227cbf968ea2caaf
APPLE <apple@APPLEs-Mac-mini.local>
2015-10-26 14:43:02 +0530

    schedule medicine page completed....

---
+++ Medico/MedicinePrescribedScheduleViewController.m
@@ -0,0 +1,82 @@
+//
+//  MedicinePrescribedScheduleViewController.m
+//  Medico
+//
+//  Created by APPLE on 26/10/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+#import "MedicinePrescribedScheduleViewController.h"
+#import "DoctorLandingPageView.h"
+
+@interface MedicinePrescribedScheduleViewController ()
+
+@end
+
+@implementation MedicinePrescribedScheduleViewController
+@synthesize doctorsInstructionTextView;
+@synthesize scheduleTimeTextView;
+@synthesize check;
+@synthesize medicineReminderButton;
+
+- (void) homePage:(id)sender{
+    DoctorLandingPageView *DoctorHome =
+    [self.storyboard instantiateViewControllerWithIdentifier:@"DoctorHome"];
+    [self.navigationController pushViewController:DoctorHome animated:YES];
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIImage *myImage = [UIImage imageNamed:@"home.png"];
+    UIBarButtonItem *homeButton = [[UIBarButtonItem alloc]  initWithImage:myImage style:UIBarButtonItemStylePlain target:self action:@selector(homePage:)];
+    NSArray *buttonArr = [[NSArray alloc] initWithObjects:homeButton, nil];
+    self.navigationItem.rightBarButtonItems = buttonArr;
+    
+    self.navigationItem.title = @"Medicine Schedule";
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleDone target:nil action:nil];
+    [[self navigationItem] setBackBarButtonItem:backButton];
+    [doctorsInstructionTextView.layer setBorderWidth:1.0];
+    [scheduleTimeTextView.layer setBorderWidth:1.0];
+    check = NO;
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)save:(id)sender {
+}
+
+- (IBAction)medicineReminder:(id)sender {
+    if(!check){
+        [medicineReminderButton setImage:[UIImage imageNamed:@"checked.png"]forState:UIControlStateNormal];
+        check = YES;
+    }
+    else if(check){
+        [medicineReminderButton setImage:[UIImage imageNamed:@"unchecked.png"]forState:UIControlStateNormal];
+        check = NO;
+        
+    }
+
+}
+
+- (IBAction)startDate:(id)sender {
+}
+
+- (IBAction)endDate:(id)sender {
+}
+@end
