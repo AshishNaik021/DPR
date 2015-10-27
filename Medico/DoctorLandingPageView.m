43a47286e7fad4bffeb35a6ec01cfb879dbfab9f
Poonam Choudhari <poonam6choudhari@gmail.com>
2015-10-27 11:13:26 +0530

    Login, Doc name

--- Medico/DoctorLandingPageView.m
+++ Medico/DoctorLandingPageView.m
@@ -20,29 +20,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.hidesBackButton = YES;
-    //    NSString *fileName = [[NSBundle mainBundle] pathForResource:@"MyDemo" ofType:@"json"];
-    //    NSString *myJson = [[NSString alloc] initWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:NULL];
-    //    NSError *error = nil;
-    //    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[myJson dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
-    //    NSArray *arr = [json valueForKeyPath:@"demo"];
-    //self.doctorNameLabel.text = [[arr objectAtIndex:0] objectForKey:@"Name"];
-    self.doctorNameLabel.text = _doctorName;
+    NSLog(@"Name:%@ Email :%@" ,_doctorName,_doctorEmail);
     
-    //[self setName];
+    [self setName];
     // Do any additional setup after loading the view.
 }
-//-(void)setName{
-//    NSLog(@"docemail %@",_doctorEmail);
-//        NSLog(@"docname %@",_doctorName);
-//        if ([_doctorEmail isEqualToString:@"ahemad@gmail.com"])
-//            self.doctorNameLabel.text=  @"Ahemad";
-//        else if ([_doctorEmail isEqualToString:@"ma@gmail.com"])
-//            self.doctorNameLabel.text =  @"Mayur Zambare";
-//        else if([_doctorEmail isEqualToString:@"kb@gmail.com"])
-//            self.doctorNameLabel.text = @"Kaustubh Bharambe";
-//        else if([_doctorEmail isEqualToString:@"r@gmail.com"])
-//            self.doctorNameLabel.text = @"Ram";
-//}
+-(void)setName{
+    //    NSString *cameFrom = [self navigationController.v]
+    NSLog(@"%@",[self.navigationController.viewControllers objectAtIndex:self.navigationController.viewControllers.count-2]);
+    
+    NSString *string = [NSString stringWithFormat:[self.navigationController.viewControllers objectAtIndex:self.navigationController.viewControllers.count-2]];
+    if ([string rangeOfString:@"LoginPage"].location == NSNotFound) {
+        self.doctorNameLabel.text = _doctorName;
+        NSLog(@"Login name was set");
+    } else {
+        NSLog(@"No need to change login name");
+        
+    }
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
