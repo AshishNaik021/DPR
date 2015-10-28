8a0efc2db218caeaf56a7055668f115206211044
APPLE <apple@APPLEs-Mac-mini.local>
2015-10-28 14:32:31 +0530

    Screen 56 created...

---
+++ Medico/MarkAndCreateAppointmentViewController.h
@@ -0,0 +1,36 @@
+//
+//  MarkAndCreateAppointmentViewController.h
+//  Medico
+//
+//  Created by APPLE on 28/10/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MarkAndCreateAppointmentViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIButton *createAppointmentButton;
+@property (weak, nonatomic) IBOutlet UITextField *slotField;
+@property (weak, nonatomic) IBOutlet UITextField *timeField;
+@property (weak, nonatomic) IBOutlet UITextField *patientField;
+- (IBAction)createAppointment:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *markAsButton;
+- (IBAction)markAs:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *markAsField;
+@property (weak, nonatomic) IBOutlet UIButton *allSlotsButton;
+- (IBAction)allSlots:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *selectedSlotsButton;
+- (IBAction)selectedSlots:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *selectedSlotsField;
+@property (weak, nonatomic) IBOutlet UIButton *selectTimeButton;
+- (IBAction)selectTime:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *selectTimeField;
+@property (weak, nonatomic) IBOutlet UIButton *wholeWeekButton;
+- (IBAction)wholeWeek:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+- (IBAction)save:(id)sender;
+
+
+@end
---
+++ Medico/MarkAndCreateAppointmentViewController.m
@@ -0,0 +1,51 @@
+//
+//  MarkAndCreateAppointmentViewController.m
+//  Medico
+//
+//  Created by APPLE on 28/10/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+#import "MarkAndCreateAppointmentViewController.h"
+
+@interface MarkAndCreateAppointmentViewController ()
+
+@end
+
+@implementation MarkAndCreateAppointmentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)createAppointment:(id)sender {
+}
+- (IBAction)markAs:(id)sender {
+}
+- (IBAction)allSlots:(id)sender {
+}
+- (IBAction)selectedSlots:(id)sender {
+}
+- (IBAction)selectTime:(id)sender {
+}
+- (IBAction)wholeWeek:(id)sender {
+}
+- (IBAction)save:(id)sender {
+}
+@end
