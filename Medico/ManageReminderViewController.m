76225e6b75f25dcbb1f416bf8fe7811d6752ddd4
APPLE <apple@APPLEs-Mac-mini.local>
2015-11-02 17:22:36 +0530

    Reminders

--- Medico/ManageReminderViewController.m
+++ Medico/ManageReminderViewController.m
@@ -37,19 +37,43 @@
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleDone target:nil action:nil];
     [[self navigationItem] setBackBarButtonItem:backButton];
     
-    NSString *fileName = [[NSBundle mainBundle] pathForResource:@"getNotifications" ofType:@"json"];
-    NSString *myJson = [[NSString alloc] initWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:NULL];
-    NSData *json = [myJson dataUsingEncoding:NSUTF8StringEncoding];
-    NSError *e;
-    
-    NSDictionary *json1 = [NSJSONSerialization JSONObjectWithData:[myJson dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&e];
-    
-    reminderArr = [json1 valueForKeyPath:@"reminders"];
-    NSLog(@"array------%@",reminderArr);
+    //    NSString *fileName = [[NSBundle mainBundle] pathForResource:@"getNotifications" ofType:@"json"];
+    //    NSString *myJson = [[NSString alloc] initWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:NULL];
+    //    NSData *json = [myJson dataUsingEncoding:NSUTF8StringEncoding];
+    //    NSError *e;
+    //
+    //    NSDictionary *json1 = [NSJSONSerialization JSONObjectWithData:[myJson dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&e];
+    //
+    //    reminderArr = [json1 valueForKeyPath:@"reminders"];
+    //    NSLog(@"array------%@",reminderArr);
     
     //reminderArr = [NSJSONSerialization JSONObjectWithData:json options:NSJSONReadingMutableContainers error:&e];
+    [self fetchNotification];
     
+}
+-(void)fetchNotification{
+    NSLog(@"fetchNotification Method is called....");
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: defaultConfigObject delegate: nil delegateQueue: [NSOperationQueue mainQueue]];
     
+    NSString *emailid = [[NSUserDefaults standardUserDefaults] valueForKey:@"loggedInEmail"];
+    NSString *urlStr = [NSString stringWithFormat:@"http://139.162.31.36:9000/getNotification?email=%@",emailid];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLResponse *response;
+    NSError *error;
+    NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    NSString *responseStr = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+    NSData *json = [responseStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *json1 = [NSJSONSerialization JSONObjectWithData:json options:NSJSONWritingPrettyPrinted error:&error];
+    reminderArr = [json1 valueForKeyPath:@"reminders"];
+    
+    //NSMutableArray *arratList = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:&error];
+    NSLog(@"Data in Array==============%@",reminderArr);
+    for (int i =0 ; i<[reminderArr count]; i++) {
+        NSLog(@"Data in Array at index %d ==============%@",i,reminderArr[i]);
+    }
 }
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
@@ -72,17 +96,17 @@
     int row = [indexPath row];
     
     if (![[[reminderArr objectAtIndex:row] objectForKey:@"title"] isEqual:[NSNull null]]) {
-       cell.reminderTitleLabel.text = [[reminderArr objectAtIndex:row] objectForKey:@"title"];
+        cell.reminderTitleLabel.text = [[reminderArr objectAtIndex:row] objectForKey:@"title"];
     }
     else
     {
-        NSLog(@"aat");
+        // NSLog(@"aat");
         cell.reminderTitleLabel.text = @"";
-        NSLog(@"baher");
+        //rNSLog(@"baher");
     }
-
     
-  //  cell.reminderTitleLabel.text = [[reminderArr objectAtIndex:row] objectForKey:@"title"];
+    
+    //  cell.reminderTitleLabel.text = [[reminderArr objectAtIndex:row] objectForKey:@"title"];
     
     cell.reminderDateLabel.text = [[reminderArr objectAtIndex:row] objectForKey:@"date"];
     cell.reminderTimeLabel.text = [[reminderArr objectAtIndex:row] objectForKey:@"time"];
@@ -110,13 +134,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
--- Medico/ReminderDetailsViewController.m
+++ Medico/ReminderDetailsViewController.m
@@ -40,8 +40,10 @@
     
     dateLabel.text = [detailReminderArray[0] objectForKey:@"date"];
     timeLabel.text = [detailReminderArray[0] objectForKey:@"time"];
-    discussionLabel.text = [detailReminderArray[0] objectForKey:@"title"];
-
+    if ([detailReminderArray[0] objectForKey:@"title"]) {
+        discussionLabel.text = [detailReminderArray[0] objectForKey:@"title"];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -50,14 +52,14 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 - (IBAction)setDateTimeAlarm:(id)sender {
 }
