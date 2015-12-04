ff134c85559a44d44e48680304a06ab062ed02f1
VitthalBobade <vitthalbobade@gmail.com>
2015-12-04 15:51:18 +0530

    Buttons linked

--- Medico/DoctorBookAppointmentViewController.h
+++ Medico/DoctorBookAppointmentViewController.h
@@ -20,5 +20,23 @@
 @property (weak, nonatomic) IBOutlet UILabel *appointmentLabel;
 @property (weak, nonatomic) IBOutlet UITextField *dateTextField;
 @property (weak, nonatomic) IBOutlet UITextField *typeTextField;
+@property (weak, nonatomic) IBOutlet UIButton *time1;
+@property (weak, nonatomic) IBOutlet UIButton *time2;
+@property (weak, nonatomic) IBOutlet UIButton *time3;
+@property (weak, nonatomic) IBOutlet UIButton *time4;
+@property (weak, nonatomic) IBOutlet UIButton *time5;
+@property (weak, nonatomic) IBOutlet UIButton *time6;
+@property (weak, nonatomic) IBOutlet UIButton *time7;
+@property (weak, nonatomic) IBOutlet UIButton *time8;
+@property (weak, nonatomic) IBOutlet UIButton *time9;
+@property (weak, nonatomic) IBOutlet UIButton *time10;
+@property (weak, nonatomic) IBOutlet UIButton *time11;
+@property (weak, nonatomic) IBOutlet UIButton *time12;
+@property (weak, nonatomic) IBOutlet UIButton *time13;
+@property (weak, nonatomic) IBOutlet UIButton *time14;
+@property (weak, nonatomic) IBOutlet UIButton *time15;
+@property (weak, nonatomic) IBOutlet UIButton *time16;
+@property (weak, nonatomic) IBOutlet UIButton *time17;
+@property (weak, nonatomic) IBOutlet UIButton *time18;
 
 @end
