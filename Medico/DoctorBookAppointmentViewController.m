117a36413f26e961e7b184d09403df0987c9a8fd
PoonamChoudhari <poonam6choudhari@gmail.com>
2015-12-17 11:42:53 +0530

    doctor complete

--- Medico/DoctorBookAppointmentViewController.m
+++ Medico/DoctorBookAppointmentViewController.m
@@ -45,6 +45,7 @@
 @synthesize time9;
 
 
+
 - (void) homePage:(id)sender{
     if([[NSUserDefaults standardUserDefaults] objectForKey:@"loggedInUserType"]){
     DoctorLandingPageView *DoctorHome =
@@ -59,8 +60,9 @@
 - (void)viewDidLoad {
     NSLog(@"DoctorBookAppointmentViewController.m");
     [super viewDidLoad];
-    UIImage *myImage = [UIImage imageNamed:@"home.png"];
+    UIImage *myImage = [UIImage imageNamed:@"ic_home.png"];
     UIBarButtonItem *homeButton = [[UIBarButtonItem alloc]  initWithImage:myImage style:UIBarButtonItemStylePlain target:self action:@selector(homePage:)];
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:120.0/255.0 green:199.0/255.0 blue:211.0/255.0 alpha:0];
     NSArray *buttonArr = [[NSArray alloc] initWithObjects:homeButton, nil];
     self.navigationItem.rightBarButtonItems = buttonArr;
     
